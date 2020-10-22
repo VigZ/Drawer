517e5e98d05b0989891728825aeaee2a6c831e2d
vigz <kyle.vigorito@gmail.com>
2020-10-22 17:08:25 -0400

    added and linked storyboard to doodad cell.

--- Drawer/DoodadCell.swift
+++ Drawer/DoodadCell.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class DoodadCell: UITableViewCell {
     
+    
+    @IBOutlet weak var doodadImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
